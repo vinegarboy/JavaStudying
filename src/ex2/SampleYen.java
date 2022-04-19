bb98de01d0c7717c503fa22462276219f1b0dff9
vinegarboy <necogusa398@gmail.com>
2022-04-19 16:17:09 +0900

    202220419課題終わり+コード整形

--- src/ex2/SampleYen.java
+++ src/ex2/SampleYen.java
@@ -1,4 +1,49 @@
 package ex2;
 
-public class SampleYen {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class SampleYen extends JFrame{
+    private SamplePanel sp;
+
+    public static void main(String args[]){
+        SampleYen sm = new SampleYen();
+    }
+    public SampleYen(){
+        super("ƒTƒ“ƒvƒ‹");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300, 300);
+        sp = new SamplePanel();
+        add(sp, BorderLayout.CENTER);
+        setVisible(true);
+    }
+
+
+    public class SamplePanel extends JPanel{
+        private ArrayList<Yen> yenlist = new ArrayList<Yen>();
+
+        public SamplePanel()
+        {
+            addMouseListener(new SampleMouseListener());
+        }
+        public void paint(Graphics g){
+            super.paint(g);
+            Iterator<Yen> it = yenlist.iterator();
+            while(it.hasNext()) {
+                Yen c = it.next();
+                c.draw(g);
+            }
+        }
+        public class SampleMouseListener extends MouseAdapter
+        {
+            public void mousePressed(MouseEvent e){
+                yenlist.add(new Yen(e.getX(), e.getY()));
+                repaint();
+            }
+        }
+    }
 }
---
+++ src/ex2/Yen.java
@@ -0,0 +1,19 @@
+package ex2;
+
+import java.awt.*;
+
+public class Yen {
+    private int x;
+    private int y;
+
+    public Yen(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public void draw(Graphics g)
+    {
+        g.setColor(Color.black);
+        g.drawChars(new char[]{'‰~'},0,1,x,y);
+    }
+}
