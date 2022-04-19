4da6953a63f159a487e729ef5669d76b7cc90c41
vinegarboy <necogusa398@gmail.com>
2022-04-19 15:46:48 +0900

    classファイル生成

---
+++ src/ex2/Person.java
@@ -0,0 +1,18 @@
+package ex2;
+
+public class Person {
+    String address;
+    String name;
+    void printAddress(){
+
+    }
+    void printName(){
+
+    }
+    void setAddress(String a){
+
+    }
+    void setName(String n){
+
+    }
+}
